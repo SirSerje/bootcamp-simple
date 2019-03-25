384d7653b6809fa67a92e83529153edf9b3be30d
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-03-25 13:12:54 +0200

    Doesn't import file apikey from private directory

--- app/index.js
+++ app/index.js
@@ -1,18 +1,36 @@
-/**
- * Application entry point
- */
+import request from 'utils/request';
 
-// Load application styles
-import 'styles/index.scss';
-import A from 'tag';
-
-console.log('!!!!', A);
-document.getElementsByTagName('body')[0].appendChild(document.createElement("B").appendChild( document.createTextNode("Water")));
 // ================================
 // START YOUR APP HERE
-// ================================
+const app = document.querySelector(".root");
 
-fetch('http://api.apixu.com/v1/current.json?key=c08f9f8a7ac34b338d1140437192203&q=Paris')
-  .then(function(response) {
-    console.log(response)
-  })
+const logo = document.createElement("img");
+logo.src = 'assets/images/113.png';
+
+const container = document.createElement("div");
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+fetch(request)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(JSON.stringify(data));
+        for (let x in data.location) {
+            document.querySelector(".root").innerHTML +=
+`<table>
+  <tr>
+    <th>${x}</th>
+  </tr>
+  <tr>
+    <td>${data.location[x]}</td>
+  </tr>
+</table>`;
+        }
+    })
+    .catch(error => console.error('Error:', error));
+// ================================
---
+++ app/utils/request.js
@@ -0,0 +1,4 @@
+// import key from '/private/apikey';
+
+const request = new Request(`http://api.apixu.com/v1/current.json?key=4a92533eecaf4511a9e152540192203&q=Kiev`);
+export default request
