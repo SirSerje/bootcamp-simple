18bc543c713bf3f11dd47a67f39190a9c9fc3ae7
SirSerje <sir.serje@gmail.com>
2019-03-25 18:56:43 +0200

    #4 review fix

--- app/utils/request.js
+++ app/utils/request.js
@@ -1,4 +1,4 @@
-import key from '/private/apikey';
+import key from '../private/apikey.js';
 
 const request = new Request(`http://api.apixu.com/v1/current.json?key=${key}&q=Kiev`);
 export default request
