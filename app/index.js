fef0960a70ba569cfa18194c590719559e57b352
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-03-25 18:04:54 +0200

    Rollback to request setting

--- app/index.js
+++ app/index.js
@@ -1,36 +1,10 @@
 import request from 'utils/request';
 
-// ================================
-// START YOUR APP HERE
-const app = document.querySelector(".root");
-
-const logo = document.createElement("img");
-logo.src = 'assets/images/113.png';
-
-const container = document.createElement("div");
-container.setAttribute('class', 'container');
-
-app.appendChild(logo);
-app.appendChild(container);
-
-// Create a request variable and assign a new XMLHttpRequest object to it.
 fetch(request)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         console.log(JSON.stringify(data));
-        for (let x in data.location) {
-            document.querySelector(".root").innerHTML +=
-`<table>
-  <tr>
-    <th>${x}</th>
-  </tr>
-  <tr>
-    <td>${data.location[x]}</td>
-  </tr>
-</table>`;
-        }
     })
     .catch(error => console.error('Error:', error));
-// ================================
--- app/utils/request.js
+++ app/utils/request.js
@@ -1,4 +1,4 @@
-// import key from '/private/apikey';
+import key from '/private/apikey';
 
-const request = new Request(`http://api.apixu.com/v1/current.json?key=4a92533eecaf4511a9e152540192203&q=Kiev`);
+const request = new Request(`http://api.apixu.com/v1/current.json?key=${key}&q=Kiev`);
 export default request
