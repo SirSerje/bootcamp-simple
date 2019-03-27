52aaf4a814b5c4be272933f68805657c2db3fc21
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-03-27 15:25:41 +0200

    Render data into HTML

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
 import request from 'utils/request';
 
 fetch(request)
-    .then((response) => {
-        return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-        console.log(JSON.stringify(data));
+        for (const dataKey in data.location) {
+            const li = createNode('ul'),
+                ul = createNode('li'),
+                body = document.querySelector("body");
+            li.innerHTML = `${dataKey}: ${data.location[dataKey]}`;
+            append(ul, li);
+            append(body, ul);
+        }
+
+        for (const key in data.current) {
+            const li = createNode('ul'),
+                ul = createNode('li'),
+                body = document.querySelector("body");
+            li.innerHTML = `${key}: ${data.current[key]}`;
+            append(ul, li);
+            append(body, ul);
+        }
     })
     .catch(error => console.error('Error:', error));
+
+function createNode(el) {
+    return document.createElement(el);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
