b5d55d780e5f6f060b5bd3ddd5cd4d2a83597908
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-03-28 15:13:19 +0200

    Create function which create button

--- app/index.js
+++ app/index.js
@@ -3,16 +3,19 @@ import request from 'utils/request';
 
 document.querySelector("#head").innerHTML = 'Weather';
 
+window.addEventListener('load', createClickButton);
 const body = document.querySelector("body");
-const btn = document.createElement("button");
-const txt = document.createTextNode('click');
-btn.appendChild(txt);
-body.appendChild(btn);
+const main = document.querySelector("#main");
 
-btn.addEventListener('click', showData);
+function createClickButton() {
+    const btn = document.createElement("button");
+    const txt = document.createTextNode('click');
+    btn.appendChild(txt);
+    body.appendChild(btn);
+    btn.addEventListener('click', showData);
+}
 
 function showData() {
-    const main = document.querySelector("#main");
     const clone = main.cloneNode(true);
     document.body.appendChild(clone);
     request()
--- app/utils/request.js
+++ app/utils/request.js
@@ -5,10 +5,8 @@ const link = `http://api.apixu.com/v1/current.json?key=${key}&q=Kiev`;
 const request = () => fetch(link).then((response) => {
     return response.json()
 }).then((data) => {
-    console.log(JSON.stringify(data));
     return document.querySelector("#main").innerHTML = JSON.stringify(data);
 }).catch(error => {
-    console.error('Error:', error);
     return document.querySelector("#main").innerHTML = JSON.stringify(error);
 });
 
