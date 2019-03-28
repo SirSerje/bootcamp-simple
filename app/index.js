c1677eae00432e572eae4febbd4600ce329fa689
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-03-28 12:53:35 +0200

    Show request by click on button multi times

--- app/index.js
+++ app/index.js
@@ -2,4 +2,18 @@ import 'styles/index.scss';
 import request from 'utils/request';
 
 document.querySelector("#head").innerHTML = 'Weather';
-request();
+
+const body = document.querySelector("body");
+const btn = document.createElement("button");
+const txt = document.createTextNode('click');
+btn.appendChild(txt);
+body.appendChild(btn);
+
+btn.addEventListener('click', showData);
+
+function showData() {
+    const main = document.querySelector("#main");
+    const clone = main.cloneNode(true);
+    document.body.appendChild(clone);
+    request()
+}
--- app/utils/request.js
+++ app/utils/request.js
@@ -3,10 +3,13 @@ import key from '../private/apikey.js'
 const link = `http://api.apixu.com/v1/current.json?key=${key}&q=Kiev`;
 
 const request = () => fetch(link).then((response) => {
-  return response.json()
+    return response.json()
 }).then((data) => {
-  console.log(JSON.stringify(data));
-    document.querySelector("#main").innerHTML = JSON.stringify(data);
-}).catch(error => console.error('Error:', error));
+    console.log(JSON.stringify(data));
+    return document.querySelector("#main").innerHTML = JSON.stringify(data);
+}).catch(error => {
+    console.error('Error:', error);
+    return document.querySelector("#main").innerHTML = JSON.stringify(error);
+});
 
 export default request
