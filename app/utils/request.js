5e17b0b5d1962069c0d81996299c056ce83c1a38
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-04-01 16:57:18 +0300

    changed sync code to async

--- app/utils/request.js
+++ app/utils/request.js
@@ -2,11 +2,23 @@ import key from '../private/apikey.js';
 
 const link = `http://api.apixu.com/v1/current.json?key=${key}&q=Kiev`;
 
-const request = () => fetch(link).then((response) => {
-    return response.json();
-}).then((data) => {
-    return document.querySelector('#main').innerHTML = JSON.stringify(data);
-}).catch(error => {
-    return document.querySelector('#main').innerHTML = JSON.stringify(error);
-});
+const request = async function myRequest() {
+    try {
+        const response = await fetch(link);
+        const data = await response.json();
+        return document.querySelector('#main').innerHTML = JSON.stringify(data);
+    } catch (error) {
+        console.log(error);
+        return document.querySelector('#main').innerHTML = JSON.stringify(error);
+    }
+};
+
+// const request = () => fetch(link).then((response) => {
+//     return response.json();
+// }).then((data) => {
+//     return document.querySelector('#main').innerHTML = JSON.stringify(data);
+// }).catch(error => {
+//     return document.querySelector('#main').innerHTML = JSON.stringify(error);
+// });
+
 export default request;
