554ab2bf91b0eb01ca94b2401b40a29a17a5d3a0
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-04-08 16:04:33 +0300

    #8 prepare to merge and pull 29-response-parser-tests

--- app/responseParser.js
+++ app/responseParser.js
@@ -1,8 +1,15 @@
-const parser = (x) => {
-    const obj = {
-        name: x.location.name,
+const parser = (jsonObject) => {
+   /* let result = '';
+    for (const property in jsonObject.location) {
+        result += `${property}: ${jsonObject.location[property]}; `;
+    }//https://stackoverflow.com/questions/957537/how-can-i-display-a-javascript-object
+    return result;*/
+    console.log(jsonObject);
+    const objectToCompare = {
+
     };
-    return JSON.stringify(obj);
+    console.log(objectToCompare);
+    return objectToCompare;
 };
 
 export default parser;
