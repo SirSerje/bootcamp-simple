2074565febcf0dfab005fc4b6bd5c3846e182baf
Vadym Shychanyn <vadim.s.yu@gmail.com>
2019-04-11 22:50:06 +0300

    Update after review

--- app/utils/dateConverter.js
+++ app/utils/dateConverter.js
@@ -1,33 +1,27 @@
 const twelveTotwenty = function(twelve) {
     
-  var strToNum = twelve.length > 6 ? +twelve.substr(0, 2) : +twelve.substr(0, 1);
-  var AmCheck = twelve.slice(-2).toLowerCase();
-  var PmCheck = twelve.slice(-2).toLowerCase();
-  var a = strToNum + 12 == 24 ? a = '00' : AmCheck == 'am' ? strToNum = '0' + strToNum : strToNum + 12; 
-  var b = strToNum > 9 ? twelve.substr(2, 3) : (strToNum <= 9 && twelve.length > 4) ? twelve.substr(2, 3) : twelve.substr(1, 3);
-  var twenty = a + b; 
+    let strToNum = twelve.length > 6 ? +twelve.substr(0, 2) : +twelve.substr(0, 1);
+    let amPmCrop = twelve.slice(-2).toLowerCase();
+    let twelveTransformTwenty = strToNum + 12 == 24 ? twelveTransformTwenty = '00' : amPmCrop == 'am' ? strToNum = '0' + strToNum : strToNum + 12; 
+    let addingMinToHour = strToNum > 9 ? twelve.substr(2, 3) : (strToNum <= 9 && twelve.length > 4) ? twelve.substr(2, 3) : twelve.substr(1, 3);
+    let twenty = twelveTransformTwenty + addingMinToHour; 
 
-    if (strToNum > 24 || twelve.length == 5) {
-      twenty = null;
-    }
+    if (strToNum > 24 || twelve.length == 5) twenty = null;
 
- 
-  return twenty;
+    return twenty;
 }
 
 const twentyToTwelve = function(twenty) {
-    
-    var strToNum = twenty.length > 4 ? +twenty.substr(0, 2) : +twenty.substr(0, 1);
-    var a = (strToNum % 12) || 12;
-    var b = (strToNum > 9) ? twenty.substr(2, 3) : (strToNum <= 9 & twenty.length > 4) ? twenty.substr(2, 3) : twenty.substr(1, 3);
-    var amPm = strToNum < 12 ? " AM" : strToNum == 24 ? " AM" : " PM";
-    var twelve = a + b + amPm;
 
-    if (strToNum < 0 || strToNum > 24 || twenty.length > 5) {
-      twelve = null;
-    }
+    let strToNum = twenty.length > 4 ? +twenty.substr(0, 2) : +twenty.substr(0, 1);
+    let twentyTransformTwelve = (strToNum % 12) || 12;
+    let addingMinToHour = (strToNum > 9) ? twenty.substr(2, 3) : (strToNum <= 9 & twenty.length > 4) ? twenty.substr(2, 3) : twenty.substr(1, 3);
+    let amPmCheck = strToNum < 12 ? " AM" : strToNum == 24 ? " AM" : " PM";
+    let twelve = twentyTransformTwelve + addingMinToHour + amPmCheck;
+
+    if (strToNum < 0 || strToNum > 24 || twenty.length > 5) twelve = null;
 
     return twelve;
-  }
+}
 
 export { twentyToTwelve, twelveTotwenty }
