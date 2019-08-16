c600db8172e9a5c5406833c2d5ae4e1cb85c15cd
SirSerje <sir.serje@gmail.com>
2019-08-16 14:57:34 +0300

    fix request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,11 @@
 const request = async (link) => {
   try {
     const response = await fetch(link);
-    
+
     return await response.json();
   } catch (error) {
     throw new Error(error);
   }
 };
+
+export default request;
